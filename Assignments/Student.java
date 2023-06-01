0846e96fc430bf330552755661e02b985da2e492
chinmai0627 <chinmaibhavya@gmail.com>
2023-06-01 18:04:17 -0500

    Assignment-2 Student

---
+++ Assignments/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    private String name;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+        public static void main(String[] args) {
+            Student student = new Student();
+            student.setName("Chinmai Kolisetty");
+            System.out.println(student.getName());
+    }
+
+}
