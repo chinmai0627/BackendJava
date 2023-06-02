3e2f1254bb67da62eed9448ecab1a8b8f385435a
chinmai0627 <chinmaibhavya@gmail.com>
2023-06-02 11:49:13 -0500

    Assignment-2 Student

---
+++ Assignments/RunTimePolymorphismAuthentication.java
@@ -0,0 +1,16 @@
+class authentication{
+    void authenticate(){
+        System.out.println("Authenticating");
+    }
+}
+class Biometric extends authentication {
+    void authenticate() { //override method
+        System.out.println("Authenticating Biometric");
+    }
+}
+class RunTimePolymorphismAuthentication{
+    public static void main(String args[]){
+        authentication a = new Biometric();
+        a.authenticate();
+        }
+        }
