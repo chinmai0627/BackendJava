00905ab00ed0ad27c470ced64903d31365929a96
chinmai0627 <chinmaibhavya@gmail.com>
2023-06-05 12:02:54 -0500

    Assignments (June 02)

--- src/Week1/FinalExample.java
+++ src/Week1/FinalExample.java
@@ -3,5 +3,17 @@ a final method that can only be accessed by
 classes within the same package.*/
 package Week1;
 
+final class finalcar {
+    final int totalprice = 27000;
+    final void carvalue(){
+        System.out.println("Final car value");
+    }
+}
+
 public class FinalExample {
+    public static void main(String[] args){
+        finalcar obj = new finalcar();
+        obj.carvalue();
+        System.out.println(obj.totalprice);
+    }
 }
