857755d06a319128a3650e13e4abeb0d8c1fe6ac
chinmai0627 <chinmaibhavya@gmail.com>
2023-06-07 11:07:01 -0500

    Assignments (June 06)

--- src/Week2/ArrayReverse.java
+++ src/Week2/ArrayReverse.java
@@ -3,7 +3,7 @@ package Week2;
 
 public class ArrayReverse {
     public static void main(String[] args) {
-        int[] arrayNumb = {10, 4, 27, 8, 16, 43, 78};
+        int[] arrayNumb = {10, 8, 27, 8, 16, 43, 78};
         int start = 0;
         int end = arrayNumb.length - 1;
 
@@ -14,13 +14,11 @@ public class ArrayReverse {
             start++;
             end--;
         }
-        revArray(arrayNumb);
-    }
-        public static void revArray ( int[] a){
-            for (int i = 0; i < a.length; i++) {
-                System.out.print(a[i] + " ");
-            }
-            System.out.println();
+            for(int i = 0; i < arrayNumb.length; i++) {
+            System.out.print(arrayNumb[i] + " ");
+        }
+        System.out.println();
+
         }
     }
 
