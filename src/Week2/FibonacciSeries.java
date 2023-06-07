0a5e2b36a5270d7378f7f6e77d954ddcfea02d24
chinmai0627 <chinmaibhavya@gmail.com>
2023-06-07 10:48:14 -0500

    Assignments (June 05)

--- src/Week2/FibonacciSeries.java
+++ src/Week2/FibonacciSeries.java
@@ -4,7 +4,16 @@ print Fibonacci Series up to the N term.
 Input: N = 10
 Output: 0 1 1 2 3 5 8 13 21 34 */
 public class FibonacciSeries {
-    public static void main(String[] args){
+    static int fib(int n) {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        int N = 10;
+        for (int i = 0; i < N; i++) {
+            System.out.print(fib(i) + " ");
+        }
 
     }
 }
