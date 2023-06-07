ffc2b5ee0709c377df6272aae91b06814b3d8c8f
chinmai0627 <chinmaibhavya@gmail.com>
2023-06-07 10:57:16 -0500

    Assignments (June 05)

--- src/Week2/FizzBuzz.java
+++ src/Week2/FizzBuzz.java
@@ -5,6 +5,21 @@ If the number is divisible by 3 , print Fizz instead of the number.
 If the number is divisible by 5 , print Buzz instead of the number.
 If the number is divisible by both 3 and 5 , print FizzBuzz instead of the number.*/
 package Week2;
-
 public class FizzBuzz {
+    //static int i =0;
+    public static void main(String[] args){
+        for(int i=1;i<=100;i++){
+            if((i % 3 == 0) && ( i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            }
+            else if(i % 3 == 0){
+                System.out.println("Fizz");
+            }
+            else if(i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else
+                System.out.println(i);
+        }
+    }
 }
