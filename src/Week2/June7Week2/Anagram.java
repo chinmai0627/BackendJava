f17cb6925dcd5e6bb0a3d87ba86ca29d4d5eb2cd
chinmai0627 <chinmaibhavya@gmail.com>
2023-06-08 22:51:07 -0500

    Assignments (June 07)

--- src/Week2/June7Week2/Anagram.java
+++ src/Week2/June7Week2/Anagram.java
@@ -1,10 +1,19 @@
 /*Given two strings, write a program to check if the two strings are anagram of each other ?*/
 package Week2.June7Week2;
-
+import java.util.Arrays;
 public class Anagram {
     public static boolean anagram(String s1, String s2) {
-        Arrays.sort(s1);
-        Arrays.sort(s2);
+        if(s1.length() != s2.length() )
+        {
+            return false;
+        }
+        else {
+            char[] ss1 = s1.toCharArray();
+            char[] ss2 = s2.toCharArray();
+            Arrays.sort(ss1);
+            Arrays.sort(ss2);
+            return Arrays.equals(ss1,ss2);
+        }
     }
     public static void main(String[] args) {
         String s1 = "heart";
