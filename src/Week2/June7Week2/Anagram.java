c0baa46dc0f86d5db1be5104b869c4effd9f0f4b
chinmai0627 <chinmaibhavya@gmail.com>
2023-06-09 09:10:49 -0500

    June 07 files updated.

--- src/Week2/June7Week2/Anagram.java
+++ src/Week2/June7Week2/Anagram.java
@@ -3,18 +3,12 @@ package Week2.June7Week2;
 import java.util.Arrays;
 public class Anagram {
     public static boolean anagram(String s1, String s2) {
-        if(s1.length() != s2.length() )
-        {
-            return false;
-        }
-        else {
             char[] ss1 = s1.toCharArray();
             char[] ss2 = s2.toCharArray();
             Arrays.sort(ss1);
             Arrays.sort(ss2);
             return Arrays.equals(ss1,ss2);
         }
-    }
     public static void main(String[] args) {
         String s1 = "heart";
         String s2 = "earth";
