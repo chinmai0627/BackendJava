c7582166144a7d3961479f33fc700a2a865b40b7
chinmai0627 <chinmaibhavya@gmail.com>
2023-06-12 14:26:50 -0500

    files added

--- src/Week2/June7Week2/Anagram.java
+++ src/Week2/June7Week2/Anagram.java
@@ -3,6 +3,8 @@ package Week2.June7Week2;
 import java.util.Arrays;
 public class Anagram {
     public static boolean anagram(String s1, String s2) {
+            s1 = s1.toLowerCase();
+            s2 = s2.toLowerCase();
             char[] ss1 = s1.toCharArray();
             char[] ss2 = s2.toCharArray();
             Arrays.sort(ss1);
@@ -10,8 +12,8 @@ public class Anagram {
             return Arrays.equals(ss1,ss2);
         }
     public static void main(String[] args) {
-        String s1 = "heart";
-        String s2 = "earth";
+        String s1 = "Heart";
+        String s2 = "Earth";
         if(anagram(s1,s2))
         {
             System.out.println("The two strings, "+ s1 +" and "+ s2+ " are anagram of each other");
